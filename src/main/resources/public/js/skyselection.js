d9e9aed6d338b5cef160ecdb6b74ce3d0db7f138
Fiona Bianchi <fiona@hiklas.co.uk>
2017-10-16 12:02:27 +0100

    Function for splitting up product data with QUnit tests

--- src/main/resources/public/js/skyselection.js
+++ src/main/resources/public/js/skyselection.js
@@ -5,3 +5,46 @@ function clickCustomerButton()
 {
     alert("Button click");
 }
+
+/**
+ * Convert a list of products into an object with keys
+ * for category and a hash of products (by ID)
+ * under those.
+ *
+ * TODO: This doesn't do any sorting, it would be better if it
+ * TODO: did.  Also it might be even better if this was all
+ * TODO: done on the server instead of here
+ *
+ */
+function convertProductListToCategoryHash(productList)
+{
+    categories = {};
+
+    if (productList != null && productList.length != 0)
+    {
+        for(index=0; index<productList.length; index++)
+        {
+            product = productList[index];
+            addProductToCategories(product, categories);
+        }
+    }
+
+    return categories;
+}
+
+
+function addProductToCategories(product, categories)
+{
+    productId = product.id;
+    category = product.category;
+
+    categoryHash = categories[category];
+
+    if( categoryHash == null )
+    {
+        categoryHash = {};
+        categories[category] = categoryHash;
+    }
+
+    categoryHash[productId] = product;
+}
